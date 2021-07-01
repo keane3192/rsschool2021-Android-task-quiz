38716b94ac5a5fe643739dd79cfb9caf3d7732dc
keane3192 <55885290+keane3192@users.noreply.github.com>
2021-07-01 13:02:45 +0300

    feat: added solution

--- app/src/main/java/com/rsschool/quiz/MainActivity.kt
+++ app/src/main/java/com/rsschool/quiz/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.rsschool.quiz
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.rsschool.quiz.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DataPass {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        OpenFragment(0)
+    }
+
+    override fun OpenFragment(quizcount: Int) {
+        val fragment = QuizFragment.newInstance(quizcount)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
+    }
+
+    override fun OpenResult(quizres: String, msg: String) {
+        val fragment = QuizResult.newInstance(quizres, msg)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, fragment)
+            .commit()
     }
 }
